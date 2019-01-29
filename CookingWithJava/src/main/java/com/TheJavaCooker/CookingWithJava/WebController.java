ff151678ca2eaaa848614c1e25af7e3bd85967dc
JaviBJ99 <javier.martinez127@gmail.com>
2019-01-29 14:28:06 +0100

    Implantación Primeros HTMLS

--- CookingWithJava/src/main/java/com/TheJavaCooker/CookingWithJava/WebController.java
+++ CookingWithJava/src/main/java/com/TheJavaCooker/CookingWithJava/WebController.java
@@ -15,7 +15,19 @@ public class WebController {
     @GetMapping("/hello-pepe")
     public String helloPepe( Model model)
     {
-        model.addAttribute("name","PepitoGrillo");
+        model.addAttribute("name","Iago");
         return "hello";
+    }
+    @GetMapping("/about")
+    public String about( Model model)
+    {
+
+        return "about.html";
+    }
+    @GetMapping("/index")
+    public String index( Model model)
+    {
+
+        return "index.html";
     }
 }
