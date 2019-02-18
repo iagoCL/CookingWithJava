e843e8e3db5012424004dffc0a727600934cd7a7
Who1ne <juan.jim.gal@gmail.com>
2019-02-18 16:52:13 +0100

    Buscar receta 1

--- CookingWithJava/src/main/java/com/TheJavaCooker/CookingWithJava/DataBase/RecetaRepository.java
+++ CookingWithJava/src/main/java/com/TheJavaCooker/CookingWithJava/DataBase/RecetaRepository.java
@@ -29,5 +29,8 @@ public interface RecetaRepository extends JpaRepository<Receta, Long>
     @Query("select r from Receta r order by r.fecha_creacion desc")
     Page<Receta> ultimasRecetas(PageRequest pageable);
 
+    @Query("select r from Receta r where r.nombre_receta = ?1 and r.tipo_plato = ?2 and r.nivel_de_dificultad = ?3")
+    List<Receta> buscarReceta(String nombre_receta, String tipo_plato, NivelDeDificultad nivel_de_dificultad);
+
     long count();//numero de recetas
 }
--- CookingWithJava/src/main/java/com/TheJavaCooker/CookingWithJava/WebController.java
+++ CookingWithJava/src/main/java/com/TheJavaCooker/CookingWithJava/WebController.java
@@ -1,13 +1,15 @@
 package com.TheJavaCooker.CookingWithJava;
 
 import com.TheJavaCooker.CookingWithJava.DataBase.*;
-import net.bytebuddy.dynamic.loading.ClassInjector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.util.Pair;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -22,13 +24,23 @@ public class WebController {
     private ImagendbRepository imagenRepository;
 
     private long usuarioActivoId = 2;
+    private List<Receta> recetasBuscadas;
 
     //todo session para almacenar usuario
 
     @GetMapping(value={"/busqueda","/buscarReceta","buscar-receta"})
     public String buscar(Model model) {
-        //todo recibir parametros del formularios y gestionarlos por POST
         return "busqueda";
+    }
+
+    @RequestMapping(value={"/formulario-buscar-receta"})
+    public String formularioBuscarReceta(Model model, @RequestParam String nombreDeLaReceta, @RequestParam String tipoDePlato, @RequestParam String nivelDificultadReceta) {        //todo cambiar a POST
+        model.addAttribute("nombreDeLaReceta", nombreDeLaReceta);
+        model.addAttribute("tipoDePlato", tipoDePlato);
+        model.addAttribute("nivelDificultadReceta", nivelDificultadReceta);
+        NivelDeDificultad nivel_de_dificultad = NivelDeDificultad.fromString(nivelDificultadReceta);
+        recetasBuscadas = database.getRecetaRepository().buscarReceta(nombreDeLaReceta, tipoDePlato, nivel_de_dificultad);
+        return "/recetas-buscadas";
     }
 
     @GetMapping(value={"/crearReceta","/crear-receta","/subir-receta","/subirReceta"})
@@ -38,19 +50,17 @@ public class WebController {
 
     @GetMapping(value={"/inicio","/index","/"})
     public String index(Model model) {
-        //todo recibir parametros del formularios y gestionarlos por POST
         return "index";
     }
 
     @GetMapping(value={"/login","/register"})
     public String login(Model model) {
-        //todo recibir parametros del formularios y gestionarlos por POST
-        //todo almacenar sesion actual
         return "login";
     }
 
     @RequestMapping(value={"/formulario-login"})
     public String formularioLogin(Model model, @RequestParam String nickLogin, @RequestParam String contrasenaLogin) {
+        //todo cambiar a POST
         model.addAttribute("nickLogin", nickLogin);
         model.addAttribute("contrasenaLogin", contrasenaLogin);
         Usuario usuario = usuarioRepository.loginValido(nickLogin, contrasenaLogin);
@@ -65,6 +75,8 @@ public class WebController {
 
     @RequestMapping(value={"/formulario-registro"})
     public String formularioRegistro(Model model, @RequestParam String nickRegistro, @RequestParam String contrasenaRegistro, @RequestParam String correoRegistro, @RequestParam String nombreRegistro) {
+        //todo cambiar a POST
+        //todo comprobar si las dos contraseñas son iguales
         model.addAttribute("nickRegistro", nickRegistro);
         model.addAttribute("contrasenaRegistro", contrasenaRegistro);
         model.addAttribute("correoRegistro", correoRegistro);
@@ -128,17 +140,21 @@ public class WebController {
 
     @GetMapping(value={"/recetas"})
     public String mostrarRecetas(Model model) {
-        //todo misRecetas-favoritas muestra las recetas favoritas del usuario en la sesion actual
-        //todo recibir por get parametros de busqueda ejecutar la busqueda
         //todo si no recibe parametros mostrar las ultimas recetas.
         //todo hacer un metodo auxiliar para gestionar las opciones
         model.addAttribute("recetas", database.getRecetaRepository().buscarTodas());
         return "recetas";
     }
 
+    @GetMapping(value={"/mis-recetas-favoritas"})
+    public String mostrarRecetasFavoritas(Model model) {
+        Usuario usuario = usuarioRepository.findById(usuarioActivoId).orElse(null);
+        model.addAttribute("recetas", usuario.getRecetasFavoritas());
+        return "recetas";
+    }
+
     @GetMapping(value={"/recetas-favoritas-{usuarioId}"})
     public String mostrarRecetasFavoritas(Model model, @PathVariable long usuarioId) {
-        //todo obtener id usuario actual
         Usuario usuario = usuarioRepository.findById(usuarioId).orElse(null);
         model.addAttribute("recetas", usuario.getRecetasFavoritas());
         return "recetas";
@@ -146,9 +162,14 @@ public class WebController {
 
     @GetMapping(value={"/recetas-creadas-{usuarioId}"})
     public String mostrarRecetasCreadas(Model model, @PathVariable long usuarioId) {
-        //todo obtener id usuario actual
         Usuario usuario = usuarioRepository.findById(usuarioId).orElse(null);
         model.addAttribute("recetas", usuario.getRecetasCreadas());
+        return "recetas";
+    }
+
+    @GetMapping(value={"/recetas-buscadas"})
+    public String mostrarRecetasBuscadas(Model model) {
+        model.addAttribute("recetas", recetasBuscadas);
         return "recetas";
     }
 
