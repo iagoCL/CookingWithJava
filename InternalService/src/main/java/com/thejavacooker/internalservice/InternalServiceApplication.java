2e513b74da86fd46a61bb4d008d8098001de8598
Who1ne <juan.jim.gal@gmail.com>
2019-03-14 01:29:41 +0100

    Método para crear archivos PDF con texto

--- InternalService/src/main/java/com/thejavacooker/internalservice/InternalServiceApplication.java
+++ InternalService/src/main/java/com/thejavacooker/internalservice/InternalServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InternalServiceApplication {
-
     public static void main(String[] args) {
+        PDFCreator.createPDF("Macarrones", "Cocer los macarrones", "Presentar perfe");
+
         SpringApplication.run(InternalServiceApplication.class, args);
     }
-
 }
